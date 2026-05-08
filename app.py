1221a949db51e851ce1fb0276d707a4fb1569c7b
Max2k5 <nguyencaominhnguyenhue1@gmail.com>
2026-05-08 18:06:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ path_nodes, best_edge_ids = [], []
 
 st.title("📍 CHÀO MỪNG ĐẾN VỚI WEBSITE VỀ LÝ THUYẾT ĐỒ THỊ(version 1)")
 st.markdown("---")
-st.info("💡 Hướng dẫn sử dụng cơ bản: Nhập tên các điểm (A, B, C...) để tạo ra các cạnh của đồ thị. Website sẽ tự động vẽ đồ thị và phân tích các tính chất toán học như đường đi ngắn nhất, Euler và Hamilton ở các mục bên dưới. Bây giờ hãy cùng nhau bắt đầu thôi!!")
+st.info("💡 Hướng dẫn sử dụng cơ bản: Nhập tên các điểm (A, B, C...) và trọng số (nếu có) để tạo ra các cạnh của đồ thị. Website sẽ tự động vẽ đồ thị và phân tích các tính chất toán học như đường đi tổng trọng số ngắn nhất, chu trình và đường đi Euler và Hamilton ở các mục bên dưới. Bây giờ hãy cùng nhau bắt đầu thôi!!")
     # --- 2. THÔNG BÁO & QUẢN LÝ ĐƯỜNG NỐI ---
 with st.expander("➕ QUẢN LÝ ĐƯỜNG NỐI", expanded=True):
     col1, col2, col3 = st.columns([1, 1, 1])
